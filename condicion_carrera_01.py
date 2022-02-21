525c5f9c71aa0df2f326add30567ff0e10ec4dd1
Kevin-KHA <201378@ids.upchiapas.edu.mx>
2022-02-21 11:21:50 -0600

    pequeños ajustes

--- condicion_carrera_01.py
+++ condicion_carrera_01.py
@@ -8,7 +8,6 @@ def recorrer_Metraje(x):
     tiempo_corriendo = random.randint(1, 7)#tiempo que recorre el circuito cada corredor
     x.dar_Relevo(tiempo_corriendo)
 
-
 class Circuito_Relevo:
     def __init__(self, total_recorridos=0):
         self.locked = threading.Lock()
@@ -23,12 +22,12 @@ class Circuito_Relevo:
             time.sleep(espera_relevo)#competidor corriendo
 
         finally:
-            if self.bandera_Corredor < 4:
+            if self.bandera_Corredor < 5:
                 print(f"El corredor#{self.bandera_Corredor} dio relevo al siguiente"+"\n")#llegó a dar relevo
             else:
-                print(f"El corredor#{self.bandera_Corredor} (último relevado) llegó a la meta" + "\n")
+                print(f"El corredor#{self.bandera_Corredor} (último relevado) llegó a la meta" + "\n") #ha llegado el ultimo
                 print(">>>Circuito completado<<<")
-            self.locked.release()
+            self.locked.release()#se queda en su puesto
 
 
 if __name__ == '__main__':
@@ -37,6 +36,6 @@ if __name__ == '__main__':
         print(i)
         time.sleep(1)
     relevo = Circuito_Relevo()
-    for x in range(4):
-        tstart = threading.Thread(target=recorrer_Metraje, args=(relevo,))
+    for x in range(5):
+        tstart = threading.Thread(target=recorrer_Metraje, args=(relevo,))#hilo de corredor
         tstart.start()
